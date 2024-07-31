05e3b84b97885a57dd40b9b4ba8ab20f33f76403
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-07-31 09:08:23 +0530

    Easy - Convert Kilometers to Miles

---
+++ Easy - Convert Kilometers to Miles.py
@@ -0,0 +1,17 @@
+# This program is to convert Kilometers to Miles using Python
+
+# Get the distance input as Kilometers in float type
+
+kilometers = float(input("Enter the distance in kilometers = "))
+
+# Convert the obtained Kilometers to Miles using the formula
+
+kilometers_to_miles_ratio = 0.621
+
+# Compute the final distance in miles
+
+miles = kilometers*kilometers_to_miles_ratio
+
+# Print the final result
+
+print(f"The stance in miles for the given kilometer distance is {miles}")
