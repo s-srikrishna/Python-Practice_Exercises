79361ffade8ee1d1fa4e5021f2e3f5aa5df08610
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-07-31 12:21:17 +0530

    Easy - Swap Two Variables

---
+++ Easy - Swap Two Variables.py
@@ -0,0 +1,14 @@
+# This program is to swap two numbers using Python
+
+# get the integer inputs for n1 and n2
+n1 = int(input())
+n2 = int(input())
+
+# create a temporary variable and swap the values
+temp = n1
+n1 = n2
+n2 = temp
+
+# print the values of n1 and n2
+print(n1)
+print(n2)
