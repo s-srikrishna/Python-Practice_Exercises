69e27c94e26d9b2d8f63b01bc3172e1a2c8d9964
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-07-31 09:11:01 +0530

    Easy - Convert Celsius to Fahrenheit

---
+++ Easy - Convert Celsius to Fahrenheit.py
@@ -0,0 +1,10 @@
+# This program is to convert Celsius to Fahrenheit using Python
+
+# Get temperature in degree celsius from the user
+celsius = float(input())
+
+# Convert it to degree fahrenheit using the formula
+fahrenheit = (celsius * 1.8) + 32
+
+# Print the degree in fahrenheit
+print(fahrenheit)
