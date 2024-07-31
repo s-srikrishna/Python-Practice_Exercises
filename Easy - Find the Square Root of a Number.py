96e8a6d970d527a9cc1fcece905d5781a418ca3a
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-07-31 09:15:05 +0530

    Easy - Find the Square Root of a Number

---
+++ Easy - Find the Square Root of a Number.py
@@ -0,0 +1,13 @@
+# This program is to find the Sqaure Root of a number using Python
+
+# Get the input from the user in float format
+
+number = float(input("Number = "))
+
+# Apply the formula for finding the sqaure root
+
+square_root = (number)**(1/2)
+
+# Print the square root of the number
+
+print(f"The square root of the given number is {square_root}")
