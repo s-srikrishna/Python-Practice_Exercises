f3da696e99abb76082fdc9f44f6106e77ab1d197
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-07-31 12:39:35 +0530

    Easy - Profit and Loss

---
+++ Easy - Profit and Loss.py
@@ -0,0 +1,8 @@
+# This program is to calculate profit and loss using Python
+
+# get two input integers for cost_price and selling_price
+cost_price=int(input())
+selling_price=int(input())
+
+# calculate profit or loss amount and print it
+print(selling_price-cost_price)
